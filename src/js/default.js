52a005ae94da9c5d8f15b450118fbf98ecd262b8
MicRofaRatOV <ps.fx@ya.ru>
2024-06-14 13:06:52 +0300

    md support

--- src/js/default.js
+++ src/js/default.js
@@ -1,5 +1,40 @@
-// Start main page
+// Activating markdown supports
+(async ({chrome, netscape}) => {
 
+    // add Safari polyfill if needed
+    if (!chrome && !netscape)
+      await import("/plugin/textarea-md/custom-elements.min.js");
+
+    const {default: HighlightedCode} =
+      await import("/plugin/textarea-md/highlighted-code-FIX.min.js");
+
+    // bootstrap a theme through one of these names
+    // https://github.com/highlightjs/highlight.js/tree/main/src/styles
+    HighlightedCode.useTheme("github");
+  })(self);
+
+
+// Hiding markdown text <pre>
+function visibleMarkdownText(is_visible) {
+  let mdText = document.getElementsByClassName("highlighted-code")[0];
+  mdText.hidden = !is_visible;
+
+  let defalutText = document.getElementById("text-div");
+  if (is_visible) {
+    defalutText.style.color = "transperent";
+  } else {
+    defalutText.style.color = "#333333";
+  }
+  
+}
+
+// Make the "Search" button small
+function smallSearchButton() {
+  let table = document.getElementsByClassName("fixed-table-toolbar")[0];
+  table.getElementsByClassName("search")[0].classList.add("input-group-sm");
+}
+
+// Start main page
 switch(window.location.pathname) {
   case "/new-bin":
     document.getElementById("new-bin-pill").click();
@@ -14,7 +49,7 @@ switch(window.location.pathname) {
     document.getElementById("home-pill").click();
 }
 
-document.getElementById("my-bins-pill").click(); // TODO: remove
+// document.getElementById("new-bin-pill").click(); // TODO: remove
 
 
 // Textarea counter
@@ -30,11 +65,18 @@ function onInput(event) {
   counter.textContent = length;
 }
 
-
-// Make the "Search" button small
-function smallSearchButton() {
-  let table = document.getElementsByClassName("fixed-table-toolbar")[0];
-  table.getElementsByClassName("search")[0].classList.add("input-group-sm");
-}
-
 setTimeout(smallSearchButton, 150);
+
+// On click to NEW BIN 
+document.querySelector("#new-bin-pill").onclick = function() {
+  visibleMarkdownText(document.querySelector("#md-support").checked);
+};
+
+// On click to "Markdown support" check
+document.querySelector("#md-support").onclick = function() {
+  if (this.checked) {
+    visibleMarkdownText(true);
+  } else {
+    visibleMarkdownText(false);
+  }
+};
